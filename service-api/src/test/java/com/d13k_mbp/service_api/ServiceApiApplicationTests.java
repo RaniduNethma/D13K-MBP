a54943b84df4be5bbb133dc01c99a1dcada15433
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-07 15:42:03 +0530

    Initialize service API project
    Initialize service API project

---
+++ service-api/src/test/java/com/d13k_mbp/service_api/ServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d13k_mbp.service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
