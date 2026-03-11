14838c70fe83c9bda6643844b28569194c45cce1
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-11 17:21:59 +0530

    Initialize Gateway Service
    Initialize Gateway Service

---
+++ gateway-service/src/test/java/com/d13k_mbp/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d13k_mbp.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
