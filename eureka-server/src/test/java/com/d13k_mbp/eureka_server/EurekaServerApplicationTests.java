f47e4daf69d77fd4773499aa85c8423233d96130
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-11 16:55:49 +0530

    Initialize Eureka Server
    Initialize Eureka Server

---
+++ eureka-server/src/test/java/com/d13k_mbp/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d13k_mbp.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
